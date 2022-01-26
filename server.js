2b7491b3b184a7c82b36b813c07b147fe1c1d53b
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-26 00:53:46 -0300

    graphiql

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
-
+const { graphqlHTTP } = require("express-graphql");
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("Welcome to my graphql api")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to my graphql api");
+});
 
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: {},
+    graphiql: true,
+  })
+);
 app.listen(3000);
 console.log("server on port 3000");
