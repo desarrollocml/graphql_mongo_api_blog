edccd618d8678ddbffe771e7bcf74e83a570faf7
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-26 22:15:07 -0300

    paso de argumentos a mutation

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -3,7 +3,15 @@ const { GraphQLString } = require("graphql");
 const register = {
   type: GraphQLString,
   description: "Register a new User",
-  resolve() {
+  args: {
+    username: { type: GraphQLString },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+    displayName: { type: GraphQLString },
+  },
+  resolve(_, args) {
+    //" _ " iria "parent" consulta dentro de otra consulta
+    console.log(args);
     return "new user created";
   },
 };
